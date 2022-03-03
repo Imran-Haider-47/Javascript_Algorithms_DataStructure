b951dd8e45a4ba4558bad2326511d0ae5650ea67
HaiderImran <imranhaider0247@gmail.com>
2022-03-03 14:49:43 +0500

    isPalindrome

---
+++ isPalindrome.js
@@ -0,0 +1,20 @@
+// Write a function to check whether a string is palindrome or not
+function isPalindrome(str1)
+{
+    if(str1.length===0)
+    {
+        return ;
+    }
+    let str2= str1.split("");
+    let j=str2.length-1;
+    for(let i=0; i < str1.length/2; i++)
+    {
+        if(str1[i]!== str2[j])
+        {
+            return false;
+        }
+        j--;
+    }
+    return true;
+}
+console.log(isPalindrome(''))

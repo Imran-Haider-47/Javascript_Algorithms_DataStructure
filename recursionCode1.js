46c127982ebfee82319b56b6217d76b1695ab6ff
HaiderImran <imranhaider0247@gmail.com>
2022-03-03 13:18:02 +0500

    fibonacci series using recursion

--- recursionCode1.js
+++ recursionCode1.js
@@ -40,9 +40,38 @@ function Factorial(n)
     return n*Factorial(n-1);
 }
 
+function productOfArray(arr1)
+{
+    if(arr1.length===0)
+    {
+        return;
+    }
+    let product=1;
+    for(let i=0; i< arr1.length; i++)
+    {
+        product= product * arr1[i];
+    }
+    return product;
+}
+
+// Write to find a fibonacci series
+function fib(n)
+{
+   if(n<=2)
+   {
+       return 1;
+   }
+    return fib(n-1) + fib(n-2);
+}
+console.log("fib = ", fib(28));
+
+console.log("Result of productOfArray() function is  : ", productOfArray([1,2,3]));
+
 console.log("Result of Factorial() function is : ", Factorial(5));
 
 console.log("\nResult of SumRange function: ",sumRange(5));
 
 console.log("\nResult of function printNumber is Below:");
 printNumber(5);
+
+
